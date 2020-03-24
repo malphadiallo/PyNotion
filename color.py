c752be47b496f4ec2463f58ce8668184debacdca
malphadiallo <47794587+malphadiallo@users.noreply.github.com>
2020-03-25 00:10:51 +0100

    Add files via upload

---
+++ color.py
@@ -0,0 +1,8 @@
+from turtle import*
+Screen = Screen()
+Screen.setup(300,300)
+Screen.bgcolor('red')
+color('green')
+style = ('arial',40, 'bold')
+write('tontonMalpha', font=style, align='center')
+hideturtle()
